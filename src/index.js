107ab9659965ae6f6c4884a1eb797dcb6927c676
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-28 12:55:41 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-  var arr1 = equation.split(' ');
-  var a = +arr1[0];
-  var b = +(arr1[3] + arr1[4]);
-  var c = +(arr1[7] + arr1[8]);
-  
-  var d = b * b - 4 * a * c;
+  let arr = equation.split(' ');
+  const a = +arr[0];
+  const b = +(arr[3] + arr[4]);
+  const c = +(arr[7] + arr[8]);
+  let d = b * b - 4 * a * c;
+  let x1 = ((- b + Math.sqrt(d))/(2 * a)).toFixed(2);
+  let x2 = ((- b - Math.sqrt(d))/(2 * a)).toFixed(2);
+  let arrOrdered = [x1, x2];
 
-  var x1 = ((- b + Math.sqrt(d))/(2 * a)).toFixed(2);
-  var x2 = ((- b - Math.sqrt(d))/(2 * a)).toFixed(2);
-  var arr2 = [x1, x2];
-
-  function func1(a, b){
-    return a - b;
-  }
-  arr2.sort(func1)
-  return arr2;
+  let compareT = (a, b) => a - b;
+  return arrOrdered.sort(compareT);
 }
